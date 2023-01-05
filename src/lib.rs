636420e496c342603b64382793ea55eb3b4e76c7
isamyakt <isamyakt17@gmail.com>
2023-01-06 01:45:38 +0530

    added account iter, pda, rent, de/serialize

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,22 @@
 use solana_program::{
-    account_info::AccountInfo,
+    account_info::{AccountInfo, next_account_info},
     entrypoint,
     entrypoint::ProgramResult,
     pubkey::Pubkey,
-    msg
+    msg, 
+    program::invoke_signed, 
+    system_instruction, 
+    borsh::try_from_slice_unchecked,
+    sysvar::{rent::Rent, Sysvar},
 };
+use std::convert::TryInto;
 
 pub mod instruction;
+pub mod state;
+
 use instruction::MovieInstruction;
+use state::MovieAccountState;
+use borsh::BorshSerialize;
 
 entrypoint!(process_instruction);
 
@@ -27,17 +36,63 @@ pub fn process_instruction(
 }
 
 pub fn add_movie_review(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
     title: String,
     rating: u8,
     description: String,
 ) -> ProgramResult {
 
-    msg!("Adding movie review...");
-    msg!("Title: {}", title);
-    msg!("Rating: {}", rating);
-    msg!("Description: {}", description);
+    // Get Account iterator
+    let account_info_iter = &mut accounts.iter();
+
+    // Get accounts
+    let initializer = next_account_info(account_info_iter)?;
+    let pda_account = next_account_info(account_info_iter)?;
+    let system_program = next_account_info(account_info_iter)?;
+
+    let (pda, bump_seed) = Pubkey::find_program_address(
+        &[
+            initializer.key.as_ref(), 
+            title.as_bytes().as_ref(),
+        ], 
+        program_id
+    );
+
+    // Calculate account size required
+    let account_len: usize = 1 + 1 + (4 + title.len()) + (4 + description.len());
+
+    // Calculate rent required
+    let rent = Rent::get()?;
+    let rent_lamports = rent.minimum_balance(account_len);
+
+    // Create the account
+    invoke_signed(
+        &system_instruction::create_account(
+            initializer.key,
+            pda_account.key,
+            rent_lamports,
+            account_len.try_into().unwrap(),
+            program_id,
+        ), 
+        &[initializer.clone(), pda_account.clone(), system_program.clone()], 
+        &[&[initializer.key.as_ref(), title.as_bytes().as_ref(), &[bump_seed]]],
+    )?;
+
+    msg!("PDA created: {}", pda);
+
+    msg!("unpacking state account");
+    let mut account_data = try_from_slice_unchecked::<MovieAccountState>(&pda_account.data.borrow()).unwrap();
+    msg!("borrowed account data");
+
+    account_data.title = title;
+    account_data.rating = rating;
+    account_data.description = description;
+    account_data.is_initialized = true;
+
+    msg!("serializing account");
+    account_data.serialize(&mut &mut pda_account.data.borrow_mut()[..])?;
+    msg!("state account serialized");
 
     Ok(())
 
