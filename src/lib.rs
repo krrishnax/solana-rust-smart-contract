6d15765978c3a2cceffa64ecbf8370bf6d33b8f4
Samyakt <samyakt@Samyakts-MacBook-Air.local>
2023-01-05 15:19:39 +0530

    my first smart contract on rust

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg
+};
+
+pub mod instruction;
+use instruction::{MovieInstruction};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult {
+
+    let instruction = MovieInstruction::unpack(instruction_data)?;
+
+    match instruction {
+        MovieInstruction::AddMovieReview { title, rating, description } => {
+            add_movie_review(program_id, accounts, title, rating, description)
+        }
+    }
+}
+
+pub fn add_movie_review(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    title: String,
+    rating: u8,
+    description: String,
+) -> ProgramResult {
+
+    msg!("Adding movie review...");
+    msg!("Title: {}", title);
+    msg!("Rating: {}", rating);
+    msg!("Description: {}", description);
+
+    Ok(())
+
+}
+
+
+
