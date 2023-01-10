de498f81cce01ca5b54fbe1dfc0634ec54e535a8
isamyakt <isamyakt17@gmail.com>
2023-01-10 13:11:00 +0530

    added comment for better understanding

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,12 @@
 use solana_program::program_error::ProgramError;
 use thiserror::Error;
 
+// We'll need errors that we can use in the following situations:
+// The update instruction has been invoked on an account that hasn't been initialized yet
+// The provided PDA doesn't match the expected or derived PDA
+// The input data is larger than the program allows
+// The rating provided does not fall in the 1-5 range
+
 #[derive(Debug, Error)]
 pub enum ReviewError{
     // Error 0
