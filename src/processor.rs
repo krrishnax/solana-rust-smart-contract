374e0faeeab2817af1048aab38b2d03158ba351c
isamyakt <isamyakt17@gmail.com>
2023-01-20 23:10:51 +0530

    created initialize_token_mint function

--- src/processor.rs
+++ src/processor.rs
@@ -4,11 +4,13 @@ use solana_program::{
     entrypoint::ProgramResult,
     msg,
     system_instruction,
-    sysvar::{rent::Rent, Sysvar},
+    sysvar::{rent::Rent, Sysvar, rent::ID as RENT_PROGRAM_ID},
     program::invoke_signed,
     borsh::try_from_slice_unchecked, 
     program_error::ProgramError, program_pack::IsInitialized,
+    system_program::ID as SYSTEM_PROGRAM_ID
 };
+use spl_token::{instruction::initialize_mint, ID as TOKEN_PROGRAM_ID};
 
 use std::convert::TryInto;
 use borsh::BorshSerialize;
@@ -35,7 +37,9 @@ pub fn process_instruction(
         },
         MovieInstruction::AddComments { comment } => {
             add_comment(program_id, accounts, comment)
-        }
+        },
+        // New instruction handled here to initialize the mint account
+        MovieInstruction::InitializeMint => initialize_token_mint(program_id, accounts),
     }
 }
 
@@ -375,6 +379,120 @@ pub fn add_comment(
         .data
         .borrow_mut()[..]
     )?;
+
+    Ok(())
+}
+
+
+// At a high level, next steps are:
+// 1. Iterate through list of accounts to extract them
+// 2. Derive token mint PDA
+// 3. Validate all of the important accounts passed in:
+//      1. Token mint account
+//      2. Mint authority account
+//      3. System program
+//      4. Token program
+//      5. Sysvar rent - the rent calculation account
+// 4. Calculate rent for the mint account
+// 5. Create the token mint PDA
+// 6. Initialize the mint account
+
+pub fn initialize_token_mint(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+
+    // The order of accounts is not arbitrary, the client will send them in this order
+    // Whoever sent in the transaction
+    let initializer = next_account_info(account_info_iter)?;
+    // Token mint PDA - derived on the client
+    let token_mint = next_account_info(account_info_iter)?;
+    // Token mint authority
+    let mint_auth = next_account_info(account_info_iter)?;
+    // System program to create a new account
+    let system_program = next_account_info(account_info_iter)?;
+    // Solana Token program address
+    let token_program = next_account_info(account_info_iter)?;
+    // System account to calcuate the rent
+    let sysvar_rent = next_account_info(account_info_iter)?;
+
+    // Derive the mint PDA again so we can validate it
+    // The seed is just "token_mint"
+    let (mint_pda, mint_bump) = Pubkey::find_program_address(&[b"token_mint"], program_id);
+    // Derive the mint authority so we can validate it
+    // The seed is just "token_auth"
+    let (mint_auth_pda, _mint_auth_bump) =
+        Pubkey::find_program_address(&[b"token_auth"], program_id);
+
+    msg!("Token mint: {:?}", mint_pda);
+    msg!("Mint authority: {:?}", mint_auth_pda);
+
+    // Validate the important accounts passed in
+    if mint_pda != *token_mint.key {
+        msg!("Incorrect token mint account");
+        return Err(ReviewError::IncorrectAccountError.into());
+    }
+
+    if *token_program.key != TOKEN_PROGRAM_ID {
+        msg!("Incorrect token program");
+        return Err(ReviewError::IncorrectAccountError.into());
+    }
+
+    if *mint_auth.key != mint_auth_pda {
+        msg!("Incorrect mint auth account");
+        return Err(ReviewError::IncorrectAccountError.into());
+    }
+
+    if *system_program.key != SYSTEM_PROGRAM_ID {
+        msg!("Incorrect system program");
+        return Err(ReviewError::IncorrectAccountError.into());
+    }
+
+    if *sysvar_rent.key != RENT_PROGRAM_ID {
+        msg!("Incorrect rent program");
+        return Err(ReviewError::IncorrectAccountError.into());
+    }
+
+    // Calculate the rent
+    let rent = Rent::get()?;
+    // We know the size of a mint account is 82 (remember it lol)
+    let rent_lamports = rent.minimum_balance(82);
+
+    // Create the token mint PDA
+    invoke_signed(
+        &system_instruction::create_account(
+            initializer.key,
+            token_mint.key,
+            rent_lamports,
+            82, // Size of the token mint account
+            token_program.key,
+        ),
+        // Accounts we're reading from or writing to 
+        &[
+            initializer.clone(),
+            token_mint.clone(),
+            system_program.clone(),
+        ],
+        // Seeds for our token mint account
+        &[&[b"token_mint", &[mint_bump]]],
+    )?;
+
+    msg!("Created token mint account");
+
+    // Initialize the mint account
+    invoke_signed(
+        &initialize_mint(
+            token_program.key,
+            token_mint.key,
+            mint_auth.key,
+            Option::None, // Freeze authority - we don't want anyone to be able to freeze!
+            9, // Number of decimals
+        )?,
+        // Which accounts we're reading from or writing to
+        &[token_mint.clone(), sysvar_rent.clone(), mint_auth.clone()],
+        // The seeds for our token mint PDA
+        &[&[b"token_mint", &[mint_bump]]],
+    )?;
+
+    msg!("Initialized token mint");
 
     Ok(())
 }
